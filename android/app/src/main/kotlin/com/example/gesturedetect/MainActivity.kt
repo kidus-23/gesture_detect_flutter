3dbccf583043db36eb777c40d8bc0697e9797948
Kidus <73060043+kidus-23@users.noreply.github.com>
2024-12-07 15:56:48 +0300

    bottom half not showing

---
+++ android/app/src/main/kotlin/com/example/gesturedetect/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gesturedetect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Gesture Detection'),
+        ),
+        body: SafeArea(
+            child: SingleChildScrollView(
+                child: Column(
+          children: [
+            _buildGestureDetector(context),
+          ],
+        ))));
+  }
+}
+
+Draggable<int> _buildDraggable() {
+  return Draggable(
+    childWhenDragging:
+        const Icon(Icons.palette, color: Colors.grey, size: 120.0),
+    feedback: const Icon(Icons.brush, color: Colors.deepOrange, size: 120.0),
+    data: Colors.deepOrange.value,
+    child: const Column(
+      children: [
+        Icon(
+          Icons.palette,
+          color: Colors.deepOrange,
+          size: 120.0,
+        ),
+        Text('Drag me below to change color')
+      ],
+    ),
+  );
+}
+
+DragTarget<int> _buildDragTarget() {
+  Color? paintedColor;
+  return DragTarget<int>(
+    onAcceptWithDetails: (colorValue) {
+      Color paintedColor = Color(colorValue as int);
+    },
+    builder: (BuildContext context, List<dynamic> candidateData,
+            List<dynamic> rejectedData) =>
+        candidateData.isEmpty
+            ? Text(
+                'Drag tp see color',
+                style: TextStyle(color: paintedColor),
+              )
+            : Text(
+                'Drag to see color change: $candidateData',
+                style: TextStyle(
+                  color: Color(candidateData[0]),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+  );
+}
+
+GestureDetector _buildGestureDetector(BuildContext context) {
+  return GestureDetector(
+      onTap: () {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text('onTap')));
+      },
+      onDoubleTap: () {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text('onTap')));
+      },
+      onLongPress: () {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text('onTap')));
+      },
+
+//  onPanUpdate: (DragUpdateDetails details) {
+//    ScaffoldMessenger.of(context)
+//        .showSnackBar(const SnackBar(content: Text('onPanUpdate: $details')));
+//  },
+
+      onVerticalDragUpdate: (DragUpdateDetails details) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('onVerticalDragUpdate')));
+      },
+      onHorizontalDragUpdate: (DragUpdateDetails details) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('onHorizontalDragUpdate')));
+      },
+      child: Container(
+          color: Colors.lime.shade100,
+          width: double.infinity,
+          padding: const EdgeInsets.all(40.0),
+          child: const Column(
+            children: [
+              Icon(Icons.access_alarm, size: 200.0),
+            ],
+          )));
+}
